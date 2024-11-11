4916b30e8ac207a320d97f80f34c395c2ec8b50d
annikaweiland <amweilan@calpoly.edu>
2024-11-11 12:04:57 -0800

    saving lab 7

--- command_line.py
+++ command_line.py
@@ -1,5 +1,17 @@
 #TASK 3
 import sys
+import convert
+
+def arg_convert_total(input: sys.argv):
+    total = 0
+    floats = []
+    for i in input:
+        num = convert.str_to_float(i)
+        floats.append(i)
+    for i in floats:
+        if i is not None:
+            total = total + i
+    return total
 
 if __name__ == '__main__':
-    print(sys.argv)
+    print(arg_convert_total(sys.argv[1:]))
